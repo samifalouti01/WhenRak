5441264b1c53409969358c7b6be150cd3bcee61a
samifalouti1 <samifalouti01@yahoo.com>
2025-01-12 00:24:52 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.3, 
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+// the scroll-to-top button
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) { 
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+// Scroll to top functionality
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+});
+
+document.querySelectorAll('.footer-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault(); 
+
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop, 
+                behavior: 'smooth' 
+            });
+        }
+    });
+});
+
+window.addEventListener('load', () => {
+    const defaultLink = document.getElementById('default-focus');
+    const navLinks = document.querySelectorAll('.nav ul li a');
+
+    if (defaultLink) {
+        defaultLink.classList.add('active');
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            navLinks.forEach(navLink => navLink.classList.remove('active'));
+
+            link.classList.add('active');
+            const targetSection = document.querySelector(link.getAttribute('href'));
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  // Redirect to mobile.html if on a mobile device
+  if (isMobileDevice()) {
+    window.location.href = "mobile/mobile.html";
+  }
